40645186eacac347cee689abf44a3588474c0874
Ahmad Zuhrival Fauzi <zuhrivalfauzi@gmail.com>
2026-07-14 10:31:33 +0700

    feat: add ETL extraction module

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,29 +1,11 @@
 import pandas as pd
 from pathlib import Path
 
-# Folder data mentah
-RAW_DATA_PATH = Path("../data/raw")
+BASE_DIR = Path(__file__).resolve().parent.parent
+RAW_DATA_PATH = BASE_DIR / "data" / "raw"
 
-# Load datasets
 students = pd.read_csv(RAW_DATA_PATH / "students.csv")
 courses = pd.read_csv(RAW_DATA_PATH / "courses.csv")
 instructors = pd.read_csv(RAW_DATA_PATH / "instructors.csv")
 enrollments = pd.read_csv(RAW_DATA_PATH / "enrollments.csv")
 quiz_results = pd.read_csv(RAW_DATA_PATH / "quiz_results.csv")
-
-# Simpan dalam dictionary
-datasets = {
-    "students": students,
-    "courses": courses,
-    "instructors": instructors,
-    "enrollments": enrollments,
-    "quiz_results": quiz_results,
-}
-
-# Preview
-if __name__ == "__main__":
-    for name, df in datasets.items():
-        print("=" * 50)
-        print(name.upper())
-        print(df.head())
-        print()
